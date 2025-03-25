14e1fc3a44431bf29d1135f78efed2821ce70b9a
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-03-25 12:14:33 +0100

    added lolcats & cowsay

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import chalk from "chalk";
+import lolcatjs from "lolcatjs";
+import cowsay from "cowsay";
 
 const Nicolas = chalk.blue("Nicolas");
 const Vianel = chalk.green("Vianel");
 const Hans = chalk.red("Hans");
 const David = chalk.yellow("David");
 
-console.log(Nicolas, Vianel, Hans, David);
+lolcatjs.fromString(`Coucou les loulous :`);
+console.log(cowsay.say({ text: Nicolas + Vianel + Hans + David }));
