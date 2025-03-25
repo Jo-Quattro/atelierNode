46b035b92a1230f5808d59fcd6e22ff81dced92a
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-03-25 11:19:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+const Nicolas = chalk.blue("Nicolas");
+const Vianel = chalk.green("Vianel");
+const Hans = chalk.red("Hans");
+const David = chalk.yellow("David");
+
+console.log(Nicolas, Vianel, Hans, David);
